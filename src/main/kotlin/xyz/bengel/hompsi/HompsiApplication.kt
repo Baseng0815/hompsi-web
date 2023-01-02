022423768495890c2924e27f82f1f9c5939fd107
Bastian Engel <bastian.engel00@gmail.com>
2023-01-02 05:00:54 +0100

    add project

---
+++ src/main/kotlin/xyz/bengel/hompsi/HompsiApplication.kt
@@ -0,0 +1,11 @@
+package xyz.bengel.hompsi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HompsiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HompsiApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/bengel/hompsi/HompsiApplicationTests.kt
@@ -0,0 +1,18 @@
+package xyz.bengel.hompsi
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class HompsiApplicationTests @Autowired constructor(val mvc: MockMvc) {
+	@Test
+	fun contextLoads() {
+
+	}
+}
